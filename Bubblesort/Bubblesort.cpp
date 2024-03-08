44b03fe9aefa8038e9d9a748890e9d56aac1664b
maskikuy <kproject305@gmail.com>
2024-03-08 15:57:00 +0700

    menambahkan procedur display

--- Bubblesort/Bubblesort.cpp
+++ Bubblesort/Bubblesort.cpp
@@ -55,3 +55,24 @@ void bubbleSortArray() { // procedur untuk mengurutkan array dnegan metode bubbl
 	} while (pass <= n - 1);  //step 5
 
 }
+
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j]; // menampilkan array
+		if (j < n - 1) {
+			cout << " -->";
+		}
+	}
+	cout << endl;
+	cout << endl;
+
+	cout << " jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
+	cout << endl;
+	cout << endl;
+
+}
