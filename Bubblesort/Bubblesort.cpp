4c76fac53e88172da450353f2ac2f2fd66bf53cf
maskikuy <kproject305@gmail.com>
2024-03-08 15:12:45 +0700

    Add project files.

---
+++ Bubblesort/Bubblesort.cpp
@@ -0,0 +1,14 @@
+// Bubblesort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
+int n;// Deklarasi variabel global n untuk menyimpan banyaknya elemen array 
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
