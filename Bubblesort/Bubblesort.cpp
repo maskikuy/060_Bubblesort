378396e97d1ba30505d93e1b27f912cb5bedeeee
maskikuy <kproject305@gmail.com>
2024-03-08 15:50:22 +0700

    output

--- Bubblesort/Bubblesort.cpp
+++ Bubblesort/Bubblesort.cpp
@@ -30,3 +30,28 @@ void input() {		//procedur untuk input
 
 	}
 }
+
+void bubbleSortArray() { // procedur untuk mengurutkan array dnegan metode bubble sort
+	int pass = 1; // step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+			if (arr[j] > arr[j + 1]) { // step 3
+				int temp;
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+
+			}
+		}
+		pass = pass + 1; //step 4
+
+		cout << "\nPass" << pass - 1 << ": "; // number of pass
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " ";	// Menampilkan data pada number off pass
+
+		}
+		cout << endl;
+	} while (pass <= n - 1);  //step 5
+
+}
